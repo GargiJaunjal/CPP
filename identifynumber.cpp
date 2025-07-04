e0bf9355325b2bfc7c4ff5ae6ab9fef435bd18d3
Gargi <gargijaunjal@gmail.com>
2025-07-05 01:46:35 +0530

    Add files via upload

---
+++ identifynumber.cpp
@@ -0,0 +1,22 @@
+//We have to identify whether the number is power of 2 or not
+#include <iostream>
+using namespace std;
+
+bool isPowerOfTwo(int n) {
+    if (n <= 0)
+        return false;
+    return (n & (n - 1)) == 0;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isPowerOfTwo(num))
+        cout << num << " is a power of 2." << endl;
+    else
+        cout << num << " is not a power of 2." << endl;
+
+    return 0;
+}
